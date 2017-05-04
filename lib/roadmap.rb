f3268d3719d102ea01be465e1a53b82799ef54a3
Drew Mace <dremac1@live.com>
2017-05-04 19:21:05 +0000

    update checkpoint submissions

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -9,4 +9,9 @@ module Roadmap
     response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: {"authorization" => @auth_token})
     JSON.parse(response.body)
   end
+  
+  def update_checkpoint(checkpoint_id)
+    response = self.class.put(api_url("checkpoint_submissions/#{checkpoint_id}"), headers: {"authorization" => @auth_token})
+    JSON.parse(response.body)
+  end
 end 
